65c4aca58c815e1e57d3c1b74dc17c7b04f05bec
Adam Clark <adam.h.clark@gmail.com>
2016-03-20 01:44:02 -0700

    Starting to extend

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react'
 
-export default class App extends Component {
+class Row extends Component {
   render() {
+    return (
+      <li key={this.props.label}>{this.props.label}</li>
+    );
+  }
+}
+
+const ROWS = [
+  {label: "Item 1"},
+  {label: "Item 2"},
+  {label: "Item 3"},
+  {label: "Item 4"},
+  {label: "Item 5"}
+];
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: []
+    };
+  }
+  update() {
+    this.setState({rows: ROWS});
+  }
+  render() {
+    const rows = [];
+    this.state.rows.forEach(function(row) {
+      rows.push(<Row label={row.label} />);
+    });
     return(
-      <h1>Hello World 3</h1>
-    )
+      <div>
+        <h1>Hello World 3</h1>
+        <div><button type="button" onClick={this.update.bind(this)}>Update</button></div>
+        <ul>{rows}</ul>
+      </div>
+    );
   }
 }
