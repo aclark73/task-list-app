33a8a0c8037ce1caf65b9237a1ad90d41330a005
Adam Clark <adam.h.clark@gmail.com>
2016-03-22 11:57:25 -1000

    wip

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 
 class Row extends Component {
+  handleClick() {
+    this.props.handleClick(this.props.label);
+  }
   render() {
+    const handleClick = this.handleClick.bind(this);
     return (
-      <li key={this.props.label}>{this.props.label}</li>
+      <li>
+        {this.props.label}
+        <button type="button" onClick={handleClick} />
+      </li>
     );
   }
 }
@@ -23,18 +30,23 @@ export default class App extends Component {
       rows: []
     };
   }
-  update() {
+  handleUpdate() {
     this.setState({rows: ROWS});
+  }
+  handleRowClick(row) {
+    console.log("Clicked on row: " + row);
   }
   render() {
     const rows = [];
+    const handleRowClick = this.handleRowClick.bind(this);
     this.state.rows.forEach(function(row) {
-      rows.push(<Row label={row.label} />);
+      rows.push(<Row key={row.label} label={row.label} handleClick={handleRowClick} />);
     });
+    const handleUpdate = this.handleUpdate.bind(this);
     return(
-      <div>
+      <div >
         <h1>Hello World 3</h1>
-        <div><button type="button" onClick={this.update.bind(this)}>Update</button></div>
+        <div><button type="button" onClick={handleUpdate}>Update</button></div>
         <ul>{rows}</ul>
       </div>
     );
